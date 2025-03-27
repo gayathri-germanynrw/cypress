5eb6f9c0917375c14fc9db1b03d6ed0402f126a6
gayat <gayathrikasi@gmail.com>
2025-03-27 12:08:20 +0100

    should and - implicit assertions

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -2,11 +2,18 @@
 
 describe("Assertions demo", ()=>{
     it("Implicit assertions", ()=>{
+        // should and -- two implicit assertions
         cy.visit("https://www.google.com/")
         cy.url().should('include','google')
-        cy.url().should('eq','https://www.google.com/')
-        cy.url().should('contain','google')
-        cy.url().should('not.contain','yahoo')
+        .should('eq','https://www.google.com/')
+        .should('contain','google')
+        .should('not.contain','yahoo')
+
+
+        cy.url().should('include','google')
+        .and('eq','https://www.google.com/')
+        .and('contain','google')
+        .and('not.contain','yahoo')
 
     })
 

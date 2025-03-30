be1e8ff769f5ce74621d1f8a135ca11377b73ab8
gayat <gayathrikasi@gmail.com>
2025-03-30 22:12:49 +0200

    comments added

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -2,16 +2,21 @@
 
 describe('Handle Dropdowns',()=>{
     it('Dropdown with Select',()=>{
+
+        //Select Drop down , so we use select 
         cy.visit('https://practice.cydeo.com/dropdown')
         cy.get('#state').select('IN')
+        // use have.value to verify selected value
         .should('have.value', 'IN');
     })
 
     it('Dropdown without Select',()=>{
+        // can't use select 
         cy.visit('https://practice.cydeo.com/dropdown')
         cy.get('#dropdownMenuLink').click()
        cy.get('a[href="https://www.google.com/"]')
         .click()
+        // use have.text
        .should('have.text','Google')
       
     })

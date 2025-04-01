a439a194094e016314734f777aa024224dab8cde
gayat <gayathrikasi@gmail.com>
2025-04-01 15:30:30 +0200

    Auto and dynamic suggested dropdown

--- cypress/e2e/Dropdowns.cy.js
+++ cypress/e2e/Dropdowns.cy.js
@@ -1,7 +1,7 @@
 
 
 describe('Handle Dropdowns',()=>{
-    it('Dropdown with Select',()=>{
+    it.skip('Dropdown with Select',()=>{
 
         //Select Drop down , so we use select 
         cy.visit('https://practice.cydeo.com/dropdown')
@@ -10,7 +10,7 @@ describe('Handle Dropdowns',()=>{
         .should('have.value', 'IN');
     })
 
-    it('Dropdown without Select',()=>{
+    it.skip('Dropdown without Select',()=>{
         // can't use select 
         cy.visit('https://practice.cydeo.com/dropdown')
         cy.get('#dropdownMenuLink').click()
@@ -20,4 +20,38 @@ describe('Handle Dropdowns',()=>{
        .should('have.text','Google')
       
     })
+
+    it.skip('Auto suggested Dropdowns',()=>{
+        
+        cy.visit('https://www.wikipedia.org/')
+        cy.get('#searchInput').type('Delhi')
+        // contains also a part of Assertion
+        cy.get('.suggestion-title').contains(' University').click()
+   })
+
+
+   it('Dynamic Dropdowns',()=>{
+        
+    cy.visit('https://www.google.com/')
+    cy.get("textarea[name='q']").type('Cypress Automation')
+    cy.wait(3000)
+    // contains also a part of Assertion
+    cy.get('div.wM6W7d>span').should('have.length',13)
+
+    cy.get('div.wM6W7d>span').each(($el, index, $list) => {
+        // $el is a wrapped jQuery element
+        if ($el.text() == 'cypress automation tutorial') {
+          // wrap this element so we can
+          // use cypress commands on it
+          cy.wrap($el).click()
+        } else {
+          // do something else
+        }
+        cy.get("textarea[name='q']").should('have.value','cypress automation tutorial')
+})
+   
+})
+
+
+    
 })
